37b9b508449e6a31f32f0e9c7eac6a319cbb2ecc
wozijimo <346302975@qq.com>
2020-12-17 23:01:26 +0800

    1213笔记作业

--- README.md
+++ README.md
@@ -17,3 +17,51 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+### Vue class1
+1. Vue-cli安装(cnp配置)
+    1. 检查node和npm是否安
+        node -v
+    2. 将npm转换为cnpm
+        npm install -g cnpm --registry=https://registry.npm.taobao.org
+    3. npm install -g @vue/cli
+
+2. Vue指令创建项目
+    1. 在硬盘上找一个文件夹放工程用的
+    2. vue create hello-world
+
+3. Vue npm 运行Vue项目指令 
+    Vue run serve
+
+4. 工程目录文件/文件夹名字
+    assets 用来放置图片
+    components 用来放置组件文件
+    App.vue  项目的入口文件，主要编写文件
+
+5. 编辑App.Vue查看样式
+    
+6. Vue模板语法
+    v-bind  用于绑定数据和元素属性
+    v-if  判断语句
+    v-for  循环语句
+    v-on  添加一个事件监听器
+    v-model  表单输入和应用状态之间的双向绑定
+7. data方法以及数据
+    return 一个数组，里面数值可以后续拿到，或者赋值
+8. methods属性及方法
+    写一些方法，便于后面的调用
+9. v-bind:class特殊处理
+10. 元素样式依然写在style里
+11. templete标签有一个限制
+    只能有一个直接子标签
+12. this
+    在方法里调用data中的数据
+
+作业
+1. 完成百度新闻 
+2. 使用Vue写朋友圈(展示,发布)
+3. 随机点名
+需要新创建,上传git hub
+git 主页  new 
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,131 @@
 <template>
+
+<!-- 只允许有一个直接子元素 -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{message}}
+
+    <!-- v-bind指令 -->
+    <span v-bind:title="message">我是span标签</span>
+
+    <!-- v-for指令 -->
+    <div v-for="(item,index) in arr" :key="index">{{item}}</div>
+
+    <!-- v-if指令 -->
+    <div v-if="false">我是if指令</div>
+    <div v-else-if="true">我是if指令 else if 分支</div>
+    <div v-else>我是if指令 else 分支</div>
+    
+
+    <!-- v-on指令 -->
+    <div v-on:click="test"></div>
+
+    <!-- v-model指令 -->
+    <input type="text" v-model="val"/>
+    <div>我是输入框的值:{{val}}</div>
+
+    <!-- 计算器 -->
+    <div class="cls-counter">
+      <input type="number" v-model.number="number1">
+      <select v-model="opt">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input type="number" v-model.number="number2">
+      <div>=</div>
+      <button v-on:click="conput()">计算</button>
+      <div>{{sum}}</div>
+    </div>
+    <div v-if="opt == `+`">我是运算结果{{number1+number2}}</div>
+    <div v-if="opt == `-`">我是运算结果{{number1-number2}}</div>
+    <div v-if="opt == `*`">我是运算结果{{number1*number2}}</div>
+    <div v-if="opt == `/`">我是运算结果{{number1/number2}}</div>
+    <div>我是运算符:{{opt}}</div>
+
+
+    <!-- 百度新闻 -->
+    <div class="newsItem" v-for="item in baidunews" :key="item.i">
+      <div v-bind:class="[item.i == 1?`cls1`:item.i == 2 ?`cls2`:item.i == 3 ? `cls3`:`cls`]">{{item.i}}</div>
+      <div>{{item.title}}</div>
+      <div>{{item.con}}</div>
+
+    </div>
+
+
+
+
+
+
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      message:"hello",
+      arr:[10,20,30,40],
+      val:"我是val",
+      opt:"+",
+      number1:0,
+      number2:0,
+      sum:0,
+      baidunews:[{
+        i:1,
+        title:"我是标题1",
+        con:"100W"
+      },{
+        i:2,
+        title:"我是标题2",
+        con:"200W"
+      },{
+        i:3,
+        title:"我是标题3",
+        con:"300W"
+      },{
+        i:4,
+        title:"我是标题4",
+        con:"400W"
+      }],
+    };
+  },
+
+    methods: {
+    test(){
+      console.log("被点击了");
+    },
+    conput(){
+      switch(this.opt){
+        case "+":
+          this.sum = this.number1+this.number2;
+        break;
+        case "-":
+          this.sum = this.number1-this.number2;
+        break;
+        case "*":
+          this.sum = this.number1*this.number2;
+        break;
+        case "/":
+          this.sum = this.number1/this.number2;
+        break;
+
+      }
+    }
   }
 }
+
+
+
+
 </script>
 
-<style>
+<style> 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +134,39 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.cls-counter{
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+}
+
+/* 百度新闻item */
+.newsItem{
+  width: 400px;
+  height: auto;
+  display: flex;
+  flex-flow: row;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #cccccc;
+}
+
+.cls{
+  color: #999999;
+}
+
+.cls1{
+  color: #A52A2A;
+}
+
+.cls2{
+  color: #FFA54F;
+}
+
+.cls3{
+  color: #FF1493;
+}
+
+
 </style>
